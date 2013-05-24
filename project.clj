37e1485fc97aa91f8ab5f3bc46dc3d4d774d1029
Hoàng Minh Thắng <p@banphim.net>
2013-05-24 16:06:44 +0700

    update to core-cl2 0.7.1.1, set version to 0.1.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chloric "0.1.11"
+(defproject chloric "0.1.12"
   :description "Clojure/Chlorine command-line watcher/compiler"
   :url "http://github.com/myguidingstar/chloric"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
                  [org.clojure/tools.cli "0.2.2"]
                  [myguidingstar/clansi "1.3.0"]
                  [chlorine "1.5.3"]
-                 [core-cl2 "0.7.1"]]
+                 [core-cl2 "0.7.1.1"]]
   :bin {:name "chloric"
         :bin-path "~/bin"
         :bootclasspath true}
