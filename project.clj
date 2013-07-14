4aecd08f0a27bb184180e43f90c5418c2acbd061
Hoàng Minh Thắng <p@banphim.net>
2013-07-15 03:18:49 +0700

    update chlorine

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojars.zcaudate/watchtower "0.1.2"]
                  [org.clojure/tools.cli "0.2.2"]
                  [myguidingstar/clansi "1.3.0"]
-                 [chlorine "1.5.4"]
+                 [chlorine "1.6.0"]
                  [core-cl2 "0.7.3"]]
   :bin {:name "chloric"
         :bin-path "~/bin"
