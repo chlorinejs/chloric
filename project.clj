aed5548d8e0a9ba6bbfae77223422932cb6a3ef8
Hoàng Minh Thắng <p@banphim.net>
2013-07-21 15:58:00 +0700

    update chlorine and core-cl2

--- project.clj
+++ project.clj
@@ -7,9 +7,9 @@
                  [org.clojars.zcaudate/watchtower "0.1.2"]
                  [org.clojure/tools.cli "0.2.2"]
                  [myguidingstar/clansi "1.3.0"]
-                 [chlorine "1.6.2"]
+                 [chlorine "1.6.3"]
                  [closure-clj "0.1.3"]
-                 [core-cl2 "0.8.0"]]
+                 [core-cl2 "0.9.0"]]
   :bin {:name "chloric"
         :bin-path "~/bin"
         :bootclasspath true}
