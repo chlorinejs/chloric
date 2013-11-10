139b8aff10ed3048b063bd9ef6c0a84ec910d758
Hoàng Minh Thắng <p@banphim.net>
2013-11-11 03:36:16 +0700

    update chlorine libraries

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chloric "0.1.15"
+(defproject chloric "0.1.15-SNAPSHOT"
   :description "Clojure/Chlorine command-line watcher/compiler"
   :url "http://github.com/chlorinejs/chloric"
   :license {:name "Eclipse Public License"
@@ -7,9 +7,9 @@
                  [org.clojars.zcaudate/watchtower "0.1.2"]
                  [org.clojure/tools.cli "0.2.2"]
                  [myguidingstar/clansi "1.3.0"]
-                 [chlorine "1.6.3"]
+                 [chlorine "1.6.4-SNAPSHOT"]
                  [closure-clj "0.1.3"]
-                 [core-cl2 "0.9.0"]]
+                 [core-cl2 "0.9.0-SNAPSHOT"]]
   :bin {:name "chloric"
         :bin-path "~/bin"
         :bootclasspath true}
